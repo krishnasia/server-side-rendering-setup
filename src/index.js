439badb5b120f9151ebb52928f6e6cb54866765a
krishna <krishnasiva019@gmail.com>
2022-12-14 16:38:33 +0530

    render to hydrate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom";
 import {App} from "./App";
 
 const homeContailer = document.getElementById("root");
-const root = createRoot(homeContailer);
-
-root.render(<App tab='name'/>)
+ReactDom.hydrate(<App tab='name'/>,homeContailer);
